dd916c377e55611297efb0baeb9526f70da2c2ae
abdullaheladly <abdullaheladly@gmail.com>
2022-04-14 11:05:43 +0200

    finsih working in this app

--- app/src/main/java/com/abdullah996/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/abdullah996/stockmarketapp/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import com.abdullah996.stockmarketapp.presentation.company_listings.NavGraphs
+import com.abdullah996.stockmarketapp.presentation.NavGraphs
 import com.abdullah996.stockmarketapp.ui.theme.StockMarketAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/com/abdullah996/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
+++ app/src/main/java/com/abdullah996/stockmarketapp/presentation/company_listings/CompanyListingsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.ActivityNavigator
+import com.abdullah996.stockmarketapp.presentation.destinations.CompanyInfoScreenDestination
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
@@ -48,7 +49,7 @@ fun CompanyListingsScreen(
                    CompanyItem(company =company,
                    modifier = Modifier.fillMaxWidth()
                        .clickable {
-                           //TODO:Navigate
+                          navigator.navigate(CompanyInfoScreenDestination(company.symbol))
                        }.padding(
                            16.dp
                        ))
